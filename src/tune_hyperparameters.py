b01d88625a214f05a969f31000b531b5b1689623
Sta <2022ac05213@wilp.bits-pilani.ac.in>
2024-08-04 23:43:47 +0100

    Added mlflow changes

--- src/tune_hyperparameters.py
+++ src/tune_hyperparameters.py
@@ -1,7 +1,11 @@
 import optuna
 from sklearn.ensemble import RandomForestRegressor
 from sklearn.model_selection import train_test_split, cross_val_score
+import mlflow
+import mlflow.sklearn
 from utils import load_data, save_model
+
+print("MLFlow Version = "+mlflow.__version__)
 
 # Load the dataset
 X, y = load_data('data/diabetes.csv')
@@ -16,14 +20,27 @@ def objective(trial):
     mse = cross_val_score(model, X_train, y_train, cv=3, scoring='neg_mean_squared_error').mean()
     return -mse
 
-if __name__ == "__main__":
+def perform_hyperparameter_tuning_and_logging():
+    mlflow.start_run(run_name="Hyperparameter Tuning Run")
     study = optuna.create_study(direction="minimize")
     study.optimize(objective, n_trials=50)
 
-    print("Best parameters: ", study.best_params)
-    
+    best_params = study.best_params
+    print("Best parameters: ", best_params)
+
     # Save the best model
     X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
-    best_model = RandomForestRegressor(**study.best_params, random_state=42)
+    best_model = RandomForestRegressor(**best_params, random_state=42)
     best_model.fit(X_train, y_train)
     save_model(best_model, 'models/best_model.pkl')
+
+    # Log parameters and metrics to MLflow
+    mlflow.log_params(best_params)
+    mse = cross_val_score(best_model, X_test, y_test, cv=3, scoring='neg_mean_squared_error').mean()
+    mlflow.log_metric("mse", mse)
+    mlflow.sklearn.log_model(best_model, "model")
+    mlflow.end_run()
+
+if __name__ == "__main__":
+    for run in range(3):  # Conduct three runs
+        perform_hyperparameter_tuning_and_logging()
