a056fdfce19089fad739b36b321ea12e3174973d
Sta <2022ac05213@wilp.bits-pilani.ac.in>
2024-08-05 23:36:13 +0100

    Fixed Lint issues

--- src/tune_hyperparameters.py
+++ src/tune_hyperparameters.py
@@ -11,15 +11,20 @@ print("MLFlow Version = "+mlflow.__version__)
 # X, y = load_data('data/diabetes.csv')
 X, y = load_data('data_git/diabetes.csv')
 
+
 def objective(trial):
-    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
+    X_train, X_test, y_train, y_test = train_test_split(
+        X, y, test_size=0.2, random_state=42)
 
     n_estimators = trial.suggest_int("n_estimators", 10, 200)
     max_depth = trial.suggest_int("max_depth", 2, 32)
 
-    model = RandomForestRegressor(n_estimators=n_estimators, max_depth=max_depth, random_state=42)
-    mse = cross_val_score(model, X_train, y_train, cv=3, scoring='neg_mean_squared_error').mean()
+    model = RandomForestRegressor(n_estimators=n_estimators,
+                                  max_depth=max_depth, random_state=42)
+    mse = cross_val_score(model, X_train, y_train,
+                          cv=3, scoring='neg_mean_squared_error').mean()
     return -mse
+
 
 def perform_hyperparameter_tuning_and_logging():
     mlflow.start_run(run_name="Hyperparameter Tuning Run")
@@ -30,17 +35,20 @@ def perform_hyperparameter_tuning_and_logging():
     print("Best parameters: ", best_params)
 
     # Save the best model
-    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
+    X_train, X_test, y_train, y_test = train_test_split(
+        X, y, test_size=0.2, random_state=42)
     best_model = RandomForestRegressor(**best_params, random_state=42)
     best_model.fit(X_train, y_train)
     save_model(best_model, 'models/best_model.pkl')
 
     # Log parameters and metrics to MLflow
     mlflow.log_params(best_params)
-    mse = cross_val_score(best_model, X_test, y_test, cv=3, scoring='neg_mean_squared_error').mean()
+    mse = cross_val_score(best_model, X_test, y_test,
+                          cv=3, scoring='neg_mean_squared_error').mean()
     mlflow.log_metric("mse", mse)
     mlflow.sklearn.log_model(best_model, "model")
     mlflow.end_run()
+
 
 if __name__ == "__main__":
     for run in range(3):  # Conduct three runs
